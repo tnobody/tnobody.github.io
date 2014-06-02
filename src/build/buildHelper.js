ef07b8a482375804044a037abce96eea7aafd1ee
tnobody <keiner.tim@gmail.com>
2014-06-03 01:05:09 +0200

    working gulp

--- src/build/buildHelper.js
+++ src/build/buildHelper.js
@@ -1,46 +1,14 @@
-// through2 is a thin wrapper around node transform streams
-var through = require('through2');
-var gutil = require('gulp-util');
-var PluginError = gutil.PluginError;
+var fs = require('fs');
+var _ = require('lodash');
 
-// Consts
-const PLUGIN_NAME = 'gulp-page-composer';
+var templateCache = {};
 
-function text2Stream(prefixText) {
-  var stream = through();
-  stream.write(prefixText);
-  return stream;
+function getTemplate(templateFile) {
+    var data = fs.readFileSync(templateFile, 'utf8')
+    var compiled = _.template(data);
+    return compiled;
 }
 
-// Plugin level function(dealing with files)
-function gulpPageComposer() {
-
-  /*if (!prefixText) {
-    throw new PluginError(PLUGIN_NAME, "Missing prefix text!");
-  }*/
-  prefixText = new Buffer(prefixText); // allocate ahead of time
-
-  // Creating a stream through which each file will pass
-  var stream = through.obj(function(file, enc, callback) {
-    if (file.isNull()) {
-       // Do nothing if no contents
-    }
-    if (file.isBuffer()) {
-        file.contents = Buffer.concat([prefixText, file.contents]);
-    }
-
-    if (file.isStream()) {
-        file.contents = file.contents.pipe(text2Stream(prefixText));
-    }
-
-    this.push(file);
-    return callback();
-
-  });
-
-  // returning the file stream
-  return stream;
+module.exports = {
+    getTemplate: getTemplate
 };
-
-// Exporting the plugin main function
-module.exports = gulpPageComposer;
--- src/content/index.md
+++ src/content/index.md
@@ -1,12 +1,16 @@
 # Hello World
 
+this text introduces to it.this text introduces to it.this text introduces to it.this text introduces to it.this text introduces to it.this text introduces to it.this text introduces to it.this text introduces to it.this text introduces to it.this text introduces to it.this text introduces to it.this text introduces to it.this text introduces to it. 
+
 - a lsit
 - of content
-	- can dow even more
-	
+	- can dow even more          
+	         
 ``` javascript
 console.log('ghsgfh');
 ```
+
+fdfdd
 
 ---
 
