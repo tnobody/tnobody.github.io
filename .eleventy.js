14f0e026ce5eea32c604b6c63bfa8f3d24137e7b
Tim Keiner <keiner.tim@gmail.com>
2020-07-31 14:30:06 +0200

    setup sample with tailwind

---
+++ .eleventy.js
@@ -0,0 +1,19 @@
+const postCss = require('postcss');
+const tailwind = require('tailwindcss');
+
+module.exports = function (eleventyConfig) {
+
+  eleventyConfig.addWatchTarget("styles/**/*.css");
+
+  eleventyConfig.addLayoutAlias('default', 'layouts/index.njk');
+  eleventyConfig.addNunjucksAsyncFilter('cssmin', (cssCode, done) => {
+    postCss([
+        tailwind({
+          plugins: [
+            require('@tailwindcss/typography')
+          ]
+        })
+      ]).process(cssCode)
+      .then(r => done(null, r.css), e => done(e, null))
+  })
+};
