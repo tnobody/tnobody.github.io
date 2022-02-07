8658f980886fb25e911032da4b60e9a2bca55b4b
Tim Keiner <keiner.tim@gmail.com>
2022-02-07 11:49:59 +0100

    fixing serveless function max limit

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,7 +12,7 @@ module.exports = {
       safelist: ["bg-yellow-200", "bg-green-200", "bg-blue-200", "bg-yellow-300", "bg-green-300", "bg-blue-300", "bg-yellow-400", "bg-green-400", "bg-blue-400", "bg-yellow-500", "bg-green-500", "bg-blue-500", "bg-yellow-600", "bg-green-600", "bg-blue-600", "bg-yellow-700", "bg-green-700", "bg-blue-700", "bg-yellow-800", "bg-green-800", "bg-blue-800", "bg-yellow-900", "bg-green-900", "bg-blue-900"]
     }
   },
-  darkMode: false, // or 'media' or 'class'
+  darkMode: 'media', // or 'media' or 'class'
   theme: {
     extend: {},
   },
